05660d2e8d60db006cf493f409511713ac41b675
Anna Savinova <anna.savinova@abbyy.com>
2019-09-13 17:17:57 +0300

    Check if the result status is 2xx #13

--- Abbyy.CloudSdk.V2.Client/OcrClient.cs
+++ Abbyy.CloudSdk.V2.Client/OcrClient.cs
@@ -391,7 +391,7 @@ namespace Abbyy.CloudSdk.V2.Client
 						.ReadAsStringAsync()
 						.ConfigureAwait(false);
 
-				if (response.StatusCode == HttpStatusCode.OK)
+				if (response.IsSuccessStatusCode)
 				{
 					try
 					{
